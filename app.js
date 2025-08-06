2924edc1a0998029af500d8693c6e4dec0e23756
Josh Lyons <josh.lyons@snhu.edu>
2025-08-06 16:23:39 -0400

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -4,14 +4,23 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./travlr/app_server/routes/index');
+var usersRouter = require('./travlr/app_server/routes/users');
+var travelRouter = require('./travlr/app_server/routes/travel');
+var apiRouter = require('./travlr/app_api/routes/index');
+
+const handlebars = require('hbs');
+// bring in the database
+require('./travlr/app_api/models/db');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/travlr/app_server', 'views'));
 app.set('view engine', 'hbs');
+
+// register handlebars partials
+handlebars.registerPartials(__dirname + '/travlr/app_server/views/partials');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +30,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
+//console.log('apiRouter:', apiRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
